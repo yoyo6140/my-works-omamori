b0bd74213b6a1ed0bd5a165d6074b817a505156c
YOYO <abc142536798@gmail.com>
2026-04-27 11:27:22 +0800

    vercel自動化部屬

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/my-works-omamori/' : '/'
+  // GitHub Pages 需要 repo name 當 publicPath；Vercel 需要 '/'
+  publicPath:
+    process.env.NODE_ENV === 'production' && process.env.VERCEL !== '1'
+      ? '/my-works-omamori/'
+      : '/',
 })
